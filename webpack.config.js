c159837838fa9c3da1c6a68f23c7a8301725b0d3
Jingjing <jingjing@Jingjing-Mas-MacBook-Pro.local>
2019-01-02 20:00:08 -0800

    add css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,25 @@ module.exports = {
     mode: "development",
     entry: path.resolve(__dirname, "./client/src/"),
     output: {
-    path: path.resolve(__dirname, "./client/dist"),
-    filename: "bundle.js"
-},
-module: {
-    rules: [
-        {
-        loader: "babel-loader",
-        test: /\.js[x]?/,
-        exclude: /node_modules/,
-        options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-            // presets: ['env', 'react']
-        }
-    }
-    ]
-},
-// module: {
-//     rules: [
-//       {
-//         test: /\.m?js$/,
-//         exclude: /(node_modules|bower_components)/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env', '@babel/preset-react']
-//           }
-//         }
-//       }
-//     ]
-//   },
-resolve: {
-    extensions: [".js", ".jsx"]
-}
+        path: path.resolve(__dirname, "./client/dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                loader: "babel-loader",
+                test: [/\.js$/, /\.jsx?$/],
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                },
+            },
+            {
+                test: /\.css$/,
+                loaders: ["style-loader", "css-loader"],
+                include: path.join(__dirname, "client"),
+            },
+        ],
+    },
 };
 
