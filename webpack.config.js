1ab39bdfda791812e40bac4496430e3e8aadcc4d
Martin Han <martinhan@martins-mbp-2.attlocal.net>
2019-01-20 18:40:57 -0800

    Edit bundle filename in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
 const path = require("path");
 module.exports = {
-    mode: "development",
-    entry: path.resolve(__dirname, "./client/src/"),
-    output: {
-        path: path.resolve(__dirname, "./client/dist"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                loader: "babel-loader",
-                test: [/\.js$/, /\.jsx?$/],
-                exclude: /node_modules/,
-                options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
-                },
-            },
-            {
-                test: /\.css$/,
-                loader: 'style-loader',
-            },
-            {
-                test: /\.css$/,
-                loader: 'css-loader',
-                include: path.join(__dirname, 'client'),
-                query: {
-                modules: true,
-                localIdentName: '[name]__[local]__[hash:base64:5]',
-                },
-            },
-        ],
-    },
+	mode: "development",
+	entry: path.resolve(__dirname, "./client/src/"),
+	output: {
+		path: path.resolve(__dirname, "./client/dist"),
+		filename: "bundle-det.js"
+	},
+	module: {
+		rules: [
+			{
+				loader: "babel-loader",
+				test: [/\.js$/, /\.jsx?$/],
+				exclude: /node_modules/,
+				options: {
+						presets: ['@babel/preset-env', '@babel/preset-react']
+				},
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader',
+			},
+			{
+				test: /\.css$/,
+				loader: 'css-loader',
+				include: path.join(__dirname, 'client'),
+				query: {
+				modules: true,
+				localIdentName: '[name]__[local]__[hash:base64:5]',
+				},
+			},
+		],
+	},
 };
 
