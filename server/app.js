47e760469e6a69e5e80b5ef52d0cfbb3ccd42144
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-02-04 21:51:52 -0800

    [refactor]

--- server/app.js
+++ server/app.js
@@ -1,31 +1,22 @@
 const express = require('express');
 const path = require('path');
 const parser = require('body-parser');
-const { getInfo } = require('../db/index.js');
 const cors = require("cors");
+const { getInfo } = require('../db/index.js');
 
 const app = express();
 
-
 app.use(cors());
 app.use(parser.json());
-app.use(parser.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname,'../client/dist')))
+app.use(parser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,'../client/dist')));
 
-
-app.get('/api/restaurant', (req,res)=>{
-    console.log('req.query, ',req.query.id)
-    getInfo(req.query.id,(err,data)=>{
-        if(err){
-            console.log(err)
-        }else{
-            res.status(200).send(data)
-        }
-    })
-})
-
-
-
-
+app.get('/api/restaurant', (req, res) => {
+  getInfo(req.query.id, (err, data) => {
+    if (err) { console.error(err); } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 module.exports = app;
