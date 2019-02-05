80921274f45fc8a0373c7299ca637e1a5e9013a4
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-02-04 21:53:23 -0800

    [refactor]

--- server/index.js
+++ server/index.js
@@ -1,11 +1,8 @@
 const app = require('./app.js')
-const port = 9002;
+const PORT = 9002;
 
-
-app.listen(port, (err)=>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log('the server is listening to port, ', port)
-    }
-})
+app.listen(PORT, (err) => {
+  if (err) { console.log(err); } else {
+    console.log('Listening on PORT', PORT);
+  }
+});
